0101ce2bea71084ae69e1d22799ae83dcae1921d
ChanIk Yun <ychoik0818@naver.com>
2025-01-25 18:31:21 +0900

    first

---
+++ src/main/java/com/ESC_club/esc/administratorController.java
@@ -0,0 +1,8 @@
+package com.ESC_club.esc;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class administratorController {
+
+}
---
+++ src/test/java/com/ESC_club/esc/EscApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ESC_club.esc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
